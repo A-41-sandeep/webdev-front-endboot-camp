4771d5983494fe316031f866aa6a9ae86002979b
sandeep.kumar_cs20@gla.ac.in <sandeepkumarsinghsks18@gmail.com>
2022-06-27 20:34:13 +0530

    js basics

---
+++ js/calculator.js
@@ -0,0 +1,12 @@
+var a=parseInt(prompt("enter first number"));
+var b=parseInt(prompt("enter another number"));
+var sum=a+b;
+var diff=a-b;
+//output over document
+document.write("sum of numbers is ",sum+" and difference between number is ",diff);
+
+//output over console
+console.log("sum of numbers is ",sum+" and difference between number is ",diff);
+
+//output over notification
+alert("sum of numbers is "+sum+" and difference between number is "+diff);
